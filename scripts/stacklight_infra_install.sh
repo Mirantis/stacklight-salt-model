a8177b5f6c9531b039c0f063c30a91dce89713f8
Simon Pasquier <spasquier@mirantis.com>
2017-03-16 15:17:19 +0100

    Deploy Sensu in addition to Nagios
    The current system reclass model doesn't allow to deploy StackLight
    without Nagios unfortunately.
    
    Change-Id: I358d706b285f97cb20a72a418eeac2c4f0f3c60f

--- scripts/stacklight_infra_install.sh
+++ scripts/stacklight_infra_install.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -x
 exec > >(tee -i /tmp/"$(basename "$0" .sh)"_"$(date '+%Y-%m-%d_%H-%M-%S')".log) 2>&1
 
+# HAProxy and Keepalived are also required but they are already deployed by the
+# OpenStack infra or the K8s scripts.
+#
 # Install the StackLight backends
 salt -C 'I@elasticsearch:server' state.sls elasticsearch.server -b 1
 salt -C 'I@influxdb:server' state.sls influxdb -b 1
 salt -C 'I@kibana:server' state.sls kibana.server -b 1
 salt -C 'I@grafana:server' state.sls grafana.server -b 1
 salt -C 'I@nagios:server' state.sls nagios.server
+salt -C 'I@sensu:server and I@rabbitmq:server' state.sls rabbitmq
+salt -C 'I@sensu:server and I@rabbitmq:server' cmd.run "rabbitmqctl cluster_status"
+salt -C 'I@redis:cluster:role:master' state.sls redis
+salt -C 'I@redis:server' state.sls redis
+salt -C 'I@sensu:server' state.sls sensu -b 1
+
 salt -C 'I@elasticsearch:client' state.sls elasticsearch.client.service
 salt -C 'I@kibana:client' state.sls kibana.client.service
 salt -C 'I@kibana:client or I@elasticsearch:client' --async service.restart salt-minion
 sleep 10
-# RabbitMQ, HAProxy and Keepalived are required but they are already installed
-# and configured if you ran the OpenStack infra script.
-salt -C 'I@redis:cluster:role:master' state.sls redis
-salt -C 'I@redis:cluster:role:slave' state.sls redis
-salt -C 'I@sensu:server' state.sls sensu -b 1
 salt -C 'I@elasticsearch:client' state.sls elasticsearch.client
 salt -C 'I@kibana:client' state.sls kibana.client
